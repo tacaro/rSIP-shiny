7e552ab1ec1b56a7d4d186804101923b2560ffc2
tacaro <51676080+tacaro@users.noreply.github.com>
2023-11-14 15:30:49 -0700

    updated with new plot and GUI updates

--- app.R
+++ app.R
@@ -1,167 +1,258 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above (in RStudio)
-#
-# 
-#
-#
-#
 
-# Required libraries
-library(shiny)    # CRAN v1.7.4
-library(ggplot2)  # CRAN v3.4.0
-library(ggthemes) # CRAN v4.2.4
-library(plotly)   # CRAN v4.10.1
-library(cowplot)  # CRAN v1.1.1 
-library(latex2exp)
-
-# Required sourced functions
-source("source/plot_rel_error.R") # plotting function for gen_time
-source("source/calculate_mu.R") # growth rate function dependency
-source("source/calculate_sigma_mu.R") # sigma mu function dependency
-source("source/create_test_dataset.R") # test dataset function
-source("source/plot_rel_error_at2H.R") # plotting function for 2F
-source("source/plotly_rel_error_at2H.R") # plotly function for 2F!
-source("source/plotly_rel_error.R") # plotly function for gen_time!
-
-#### UI
-
-# Define UI for application
-ui <- fluidPage(
-  theme = bslib::bs_theme(bootswatch = "united"),
-  # Application title
-  titlePanel("Raman SIP Incubation App"),
-  # Set the layout:
-  tabsetPanel(
-  tabPanel("Plots",
-        fluidRow(
-          textInput("fls", label = "Label strengths (%), comma separated", value = "5, 10, 15, 20, 25, 30, 35, 40, 45, 50"),
-          textInput("inc_times", label = "Incubation Times (days), comma separated", value = "1, 5, 7, 30, 60, 100"),
-          sliderInput("assim_a", label = "Hydrogen Assimilation Efficiency (a)", value = 0.85, min = 0, max = 1),
-          textInput("sassim_a", label = "Uncertainty in hydrogen assimilation efficiency", value = 0.017),
-          textInput("sfl", label = "Uncertainty of Label Strength (%)", value = 0),
-          textInput("sf2", label = "Uncertainty of Raman 2H measurements (%)", value = 2.5),
-          numericInput("dt", label = "Highlighted Incubation Time", value = 30),
-          numericInput("f_label", label = "Highlighted Tracer Strength (%)", value = 30),
-          sliderInput("xlimits_gen", label = "Plot 1 x-limits", value = c(0, 300), min = 0, max = 1000),
-          sliderInput("xlimits_2F", label = "Plot 2 x-limits", value = c(0, 60), min = 0, max = 100)
-        ),
-      # plots
-      fluidRow(
-        plotOutput("p_rel_error"),
-        width = 8,
-        height = 20
-      )
-  ),
-  tabPanel("Interactive Plots",
-           withMathJax(
-             plotlyOutput("p_rel_error_plotly")),
-             plotlyOutput("p_rel_error_plotly_at2H")
-           ),
-  tabPanel("About this App",
-           fluidRow(
-             textOutput("about_page")
-             )
-           )
-)
-)
-
-
-
-#### SERVER
-
-# Define server logic required to plot
-server <- function(input, output) {
-  # Define reactive things
-  # A greeting (deprecated)
-  concat_str <- reactive({paste("A", input$dt, "day incubation time with", input$f_label, "% D2O:")})
+if (interactive()) {
   
-  # A numeric vector of label strengths for test data (TD)
-  fls_num <- reactive({as.numeric(str_split_1(input$fls, pattern = ","))})
+  library(shiny)     # CRAN v1.7.4
+  library(ggplot2)   # CRAN v3.4.0
+  library(ggthemes)  # CRAN v4.2.4
+  library(plotly)    # CRAN v4.10.1
+  library(cowplot)   # CRAN v1.1.1 
+  library(latex2exp) # CRAN v0.9.6
+  library(DT)        # CRAN v0.30
   
-  # A numeric vector of incubation times for TD
-  incs_num <- reactive({as.numeric(str_split_1(input$inc_times, pattern = ","))})
+  # Required sourced functions
+  source("source/plot_rel_error.R") # plotting function for gen_time
+  source("source/calculate_mu.R") # growth rate function dependency
+  source("source/calculate_sigma_mu.R") # sigma mu function dependency
+  source("source/create_test_dataset.R") # test dataset function
+  source("source/plot_rel_error_at2H.R") # plotting function for 2F
+  source("source/plotly_rel_error_at2H.R") # plotly function for 2F!
+  source("source/plotly_rel_error.R") # plotly function for gen_time!
+  source("source/plot_rel_error_inc.R") # plotting function for multiple incubation times
+  source("source/tabulate_data.R") # summarize the results of the model
   
-  # Assimilation efficiency and error in assimilation efficiency for TD
-  assim_a_dbl <- reactive({as.numeric(input$assim_a)})
-  sassim_a_dbl <- reactive({as.numeric(input$sassim_a)})
+  #### UI
   
-  # error in isotope measurements for label (sfl) and raman (sf2)
-  sfl_dbl <- reactive({as.numeric(input$sfl)})
-  sf2_dbl <- reactive({as.numeric(input$sf2)})
-  
-  # Create simulated dataset from reactive objects
-  simulated_data <- reactive({
-    create_test_dataset(
-      # input reactive objects as function arguments
-      a = assim_a_dbl(),
-      sigma_a = sassim_a_dbl(),
-      sigma_FL = sfl_dbl(),
-      sigma_F2 = sf2_dbl(),
-      FL = fls_num(),
-      dt = incs_num()
-    )
-  })
-  
-  
-  # Define outputs
-  # Print statements
-  output$fls <- renderPrint({fls_num()})
-  output$dts <- renderPrint({incs_num()})
-  output$sassim_a <- renderPrint({sassim_a_dbl()})
-  output$assim_a <- renderPrint({assim_a_dbl()})
-  output$sfl <- renderPrint({sfl_dbl()})
-  output$sf2 <- renderPrint({sf2_dbl()})
-  
-  # Plot output
-  output$p_rel_error <- renderPlot({
-    cowplot::plot_grid(
-    plot_rel_error(
-      simulated_data(),
-      d_t = input$dt,
-      f_label = input$f_label,
-      xlimits = input$xlimits_gen,
-      include_legend = TRUE
-      ),
-    plot_rel_error_at2H(
-      test_dataset = simulated_data(),
-      d_t = input$dt,
-      f_label = input$f_label,
-      xlimits = input$xlimits_2F,
-      include_legend = TRUE,
-      include_caption = TRUE
-    ),
-    ncol = 2
-    )
-  })
-
-  output$p_rel_error_plotly_at2H <- renderPlotly({
-    plotly_rel_error_at2H(
-      simulated_data(),
-      d_t = input$dt
-    )
-    })
-  
-  output$p_rel_error_plotly <- renderPlotly({
-    plotly_rel_error(
-      simulated_data(),
-      d_t = input$dt,
-      f_label = input$f_label,
-      xlimits = input$xlimits_gen
-    )
-  })
-
-  
-  # Define about page
-  output$about_page <- renderText({
-    "This app was designed by Tristan Caro at the University of Colorado Boulder. Its purpose is to allow researchers
-    interested in applying deuterium stable isotope probing (SIP) to a sample and quantifying microbial growth rates using
-    Raman spectroscopy. 
+  # Define UI for application
+  ui <- fluidPage(
+    # Set the theme
+    theme = bslib::bs_theme(bootswatch = "yeti"),
     
-    I am happy to answer questions! tristan.caro@colorado.edu"
+    # Application title
+    titlePanel("Shiny rSIP"),
+    
+    # define the layout
+    sidebarLayout(
+      # what's in the sidebar (left of page)
+      sidebarPanel(
+        textInput("fls", label = "Label strengths (%), comma separated", value = "5, 10, 15, 20, 25, 30, 35, 40, 45, 50"),
+        textInput("inc_times", label = "Incubation Times (days), comma separated", value = "1, 5, 7, 30, 60, 100"),
+        sliderInput("assim_a", label = "Hydrogen Assimilation Efficiency (a)", value = 0.85, min = 0, max = 1),
+        textInput("sassim_a", label = "Uncertainty in hydrogen assimilation efficiency", value = 0.017),
+        textInput("sfl", label = "Uncertainty of Label Strength (%)", value = 0),
+        textInput("sf2", label = "Uncertainty of Raman 2H measurements (%)", value = 2.5),
+        selectInput("f_label", "Highlighted Label Strength", choices = c(35)),
+        selectInput("dt", "Highlighted Incubation Time", choices = c(30)),
+        #numericInput("dt", label = "Highlighted Incubation Time", value = 30),
+        #numericInput("f_label", label = "Highlighted Label Strength (%)", value = 30),
+        sliderInput("xlimits_gen", label = "Plot 1 x-limits", value = c(0, 300), min = 0, max = 1000),
+        sliderInput("xlimits_2F", label = "Plot 2 x-limits", value = c(0, 60), min = 0, max = 100)
+      ),
+      
+      # what's in the main panel (center of page):
+      mainPanel(
+        tabsetPanel(
+          tabPanel(
+            "Plots",
+            fluidRow(),
+             # top two plots:
+             fluidRow(
+               plotOutput("p_rel_error"),
+               width = 8,
+               height = 20
+             ),
+             # bottom plot:
+             fluidRow(
+               plotOutput("p_rel_error_inc"),
+               width = 8,
+               height = 20
+             ),
+          ),
+          
+          # tabulated output
+          tabPanel(
+            title = "Table Output",
+            h6("Please be patient - it may take a moment to render the tables."),
+            h3("Summary of Results"),
+            downloadButton(outputId = "downloadtabulated",
+                           label = "Download Summary"),
+            tableOutput(outputId = "tabulated_results"),
+            h3("Explore Model Output"),
+            # Button
+            downloadButton(outputId = "downloadmodeldata", 
+                           label = "Download Model Output"),
+            # DT
+            tableOutput(outputId = "model_output")
+          ),
+          
+          # plotly plots
+          tabPanel(
+            "Interactive Plots",
+            withMathJax(
+              plotlyOutput("p_rel_error_plotly")),
+            plotlyOutput("p_rel_error_plotly_at2H")
+          ),
+          
+          # howto .md file
+          tabPanel(
+            "How-To",
+            fluidRow(includeMarkdown("howto.md"))
+          ),
+          
+          # about .md file
+          tabPanel(
+            "About",
+            fluidRow(
+              includeMarkdown("about.md")
+            )
+          )
+        )
+      )
+    )
+  )
+  
+  
+  
+  #### SERVER
+  
+  # Define server logic required to plot
+  server <- function(input, output) {
+
+    # define observe that allows updating the label choices based off user input
+    observe({
+      fl_choices <- as.numeric(str_split_1(input$fls, pattern = ","))
+      print(fl_choices)
+      # Can use character(0) to remove all choices
+      if (is.null(fl_choices))
+        fl_choices <- character(0)
+
+      #### This breaks the app!! Why ?? :c
+      # updateSelectInput(session = session,
+      #                   inputId = "f_label",
+      #                   choices = fl_choices
+      # )
     })
+
+    
+    
+    
+    # Define reactive things
+    # A greeting (deprecated)
+    concat_str <- reactive({paste("A", input$dt, "day incubation time with", input$f_label, "% D2O:")})
+    fls_num <- reactive({as.numeric(str_split_1(input$fls, pattern = ","))})
+    
+    # A numeric vector of incubation times for TD
+    incs_num <- reactive({as.numeric(str_split_1(input$inc_times, pattern = ","))})
+    
+    # Assimilation efficiency and error in assimilation efficiency for TD
+    assim_a_dbl <- reactive({as.numeric(input$assim_a)})
+    sassim_a_dbl <- reactive({as.numeric(input$sassim_a)})
+    
+    # error in isotope measurements for label (sfl) and raman (sf2)
+    sfl_dbl <- reactive({as.numeric(input$sfl)})
+    sf2_dbl <- reactive({as.numeric(input$sf2)})
+    
+    # Create simulated dataset reactive object from other reactives
+    simulated_data <- reactive({
+      create_test_dataset(
+        # input reactive objects as function arguments
+        a = assim_a_dbl(),
+        sigma_a = sassim_a_dbl(),
+        sigma_FL = sfl_dbl(),
+        sigma_F2 = sf2_dbl(),
+        FL = fls_num(),
+        dt = incs_num()
+      )
+    })
+    
+    # create an outcome summary reactive from the simulated_data reactive
+    outcome_summary <- reactive({
+      tabulate_data(simulated_data(),
+                    d_t = input$dt,
+                    f_label = input$f_label)
+    })
+    
+    
+    # Define outputs
+    # Print statements
+    output$fls <- renderPrint({fls_num()})
+    output$dts <- renderPrint({incs_num()})
+    output$sassim_a <- renderPrint({sassim_a_dbl()})
+    output$assim_a <- renderPrint({assim_a_dbl()})
+    output$sfl <- renderPrint({sfl_dbl()})
+    output$sf2 <- renderPrint({sf2_dbl()})
+    
+    # Plot output
+    output$p_rel_error <- renderPlot({
+      cowplot::plot_grid(
+      plot_rel_error(
+        simulated_data(),
+        d_t = input$dt,
+        f_label = input$f_label,
+        xlimits = input$xlimits_gen,
+        include_legend = TRUE
+        ),
+      plot_rel_error_at2H(
+        test_dataset = simulated_data(),
+        d_t = input$dt,
+        f_label = input$f_label,
+        xlimits = input$xlimits_2F,
+        include_legend = TRUE,
+        include_caption = TRUE
+      ),
+      ncol = 2
+      )
+    })
+  
+    output$p_rel_error_plotly_at2H <- renderPlotly({
+      plotly_rel_error_at2H(
+        simulated_data(),
+        d_t = input$dt
+      )
+      })
+    
+    output$p_rel_error_plotly <- renderPlotly({
+      plotly_rel_error(
+        simulated_data(),
+        d_t = input$dt,
+        f_label = input$f_label,
+        xlimits = input$xlimits_gen
+      )
+    })
+    
+    output$p_rel_error_inc <- renderPlot({
+      plot_rel_error_inc(
+        simulated_data(),
+        d_t = input$dt,
+        f_label = input$f_label,
+        xlimits = input$xlimits_gen,
+        include_legend = TRUE
+      )
+    })
+    
+   
+    
+    # output a DT ---
+    output$tabulated_results <- renderTable(outcome_summary())
+    output$model_output <- renderTable(simulated_data())
+    
+    # downloadable csv of dataset ---
+    output$downloadmodeldata <- downloadHandler(
+      filename = "rSIP_model_output.csv",
+      content = function(file) {
+        write.csv(simulated_data(), file, row.names = FALSE)
+      }
+    )
+    
+    output$downloadtabulated <- downloadHandler(
+      filename = "rSIP_summary.csv",
+      content = function(file) {
+        write.csv(outcome_summary(), file, row.names = FALSE)
+      }
+    )
+    
+  }
+  
+  # Run the application 
+  shinyApp(ui = ui, server = server)
+
 }
-
-# Run the application 
-shinyApp(ui = ui, server = server)
-
